5c9aff396c0dbbfe483cabd0d7be15bf4627496b
Tarun Singh <tarun.singh.7801@gmail.com>
2026-05-20 16:01:29 +0530

    removed stats

--- src/componetns/pages/HomePage.jsx
+++ src/componetns/pages/HomePage.jsx
@@ -11,7 +11,7 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      <StatsSection />
+      {/* <StatsSection /> */}
       <Services />
       <WhyChooseUs />
       <Advantages />
